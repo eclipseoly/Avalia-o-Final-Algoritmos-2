d54173348807fe3cc49242c280ce16f449b21173
eclipseoly <cuzcuzcomqueijo2007@gmail.com>
2025-11-20 23:39:51 -0300

    q1continuacao

--- q1distanciapormedia.c
+++ q1distanciapormedia.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+//organizei assim só pra poder ler o main primeiro :]
 void ler();
-float media();
+void media();
+float mediaPorNum();
+void ordenar();
 int main(){
     int n = 0;
     float med = 0;
@@ -13,19 +16,29 @@ int main(){
     }while(n > 1000);
     int vet[n];
     ler(n,vet);
+    media(n,vet,&med);
     return 0;
 }
-float media(int n, int *ptr, float *med){
+float mediaPorNum(int n, float *meds, int *vet, float med){
+    for (int i = 0; i < n; i++){
+        *(meds+i)=(float)*(vet+i)/med;
+    }
+    
+}
+void ordenar(){
+
+}
+void media(int n, int *vet, float *med){
     int soma=0;
     for (int i = 0; i < n; i++){
-        soma+= *(ptr+i);
+        soma+=*(vet+i);
     }
     *med = (float)soma/n;
 }
-void ler(int n, int *ptr){
+void ler(int n, int *vet){
     for (int i = 0; i < n; i++){
         printf("Num %d:",i);
-        scanf("%d",(ptr+i));
+        scanf("%d",(vet+i));
     }
     
 }
