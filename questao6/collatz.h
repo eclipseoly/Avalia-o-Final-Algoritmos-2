2b7e8438dd78eb2fdec6b3bc4cb737ff2cb99969
eclipseoly <cuzcuzcomqueijo2007@gmail.com>
2025-11-22 19:06:25 -0300

    questao 6 inicio

---
+++ questao6/collatz.h
@@ -0,0 +1,3 @@
+void collatz(int n){
+    
+}
---
+++ questao6/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "collatz.h"
+int main(){
+    int n=0;
+    do{
+        printf("Num:");
+        scanf("%d",&n);
+    } while (n<=1000000 || n<0);
+    collatz(n);
+    return 0;
+}
